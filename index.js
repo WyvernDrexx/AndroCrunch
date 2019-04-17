308657050cfc9e0cc11b6a6c27c5fe624003c6cb
Roscrach <lilaprashadsharma@gmail.com>
2019-04-17 21:07:33 +0530

    Added root route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require("./imports/express");
-
+const PORT = process.env.PORT || 3000;
 
 server.get("/", (req, res) => {
     res.render("index");
+});
+
+server.listen(PORT, process.env.IP, () => {
+    console.log("Listening....");
 });
