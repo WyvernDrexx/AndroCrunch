7bbacd7eb7ecaa0dd7a712cd6eccca8028ba7684
Roscrach <lilaprashadsharma@gmail.com>
2019-04-18 14:53:28 +0530

    Desing on mid section

--- public/js/main.js
+++ public/js/main.js
@@ -1,73 +1,73 @@
 function sleep(sleepDuration) {
-    var now = new Date().getTime();
-    while (new Date().getTime() < now + sleepDuration) {
-        /* do nothing */
-    }
+  var now = new Date().getTime();
+  while (new Date().getTime() < now + sleepDuration) {
+    /* do nothing */
+  }
 }
-
-
 // Scroll effect ------------------------------------------------
-(function($) {
+(function ($) {
 
-    /**
-     * Copyright 2012, Digital Fusion
-     * Licensed under the MIT license.
-     * http://teamdf.com/jquery-plugins/license/
-     *
-     * @author Sam Sehnert
-     * @desc A small plugin that checks whether elements are within
-     *     the user visible viewport of a web browser.
-     *     only accounts for vertical position, not horizontal.
-     */
-  
-    $.fn.visible = function(partial) {
-      
-        var $t            = $(this),
-            $w            = $(window),
-            viewTop       = $w.scrollTop(),
-            viewBottom    = viewTop + $w.height(),
-            _top          = $t.offset().top,
-            _bottom       = _top + $t.height(),
-            compareTop    = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
-      
-      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-  
-    };
-      
-  })(jQuery);
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function (partial) {
+
+    var $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+})(jQuery);
 
 
-  $(window).scroll(function(event) {
-  
-    $(".module").each(function(i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-        el.addClass("come-in"); 
-      } 
-    });
-    
+$(window).scroll(function (event) {
+
+  $(".module").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
   });
 
+});
 
-  var win = $(window);
+
+var win = $(window);
 var allMods = $(".module");
 
 // Already visible modules
-allMods.each(function(i, el) {
+allMods.each(function (i, el) {
   var el = $(el);
   if (el.visible(true)) {
-    el.addClass("already-visible"); 
-  } 
+    el.addClass("already-visible");
+  }
 });
 
-win.scroll(function(event) {
-  
-  allMods.each(function(i, el) {
+win.scroll(function (event) {
+
+  allMods.each(function (i, el) {
     var el = $(el);
     if (el.visible(true)) {
-      el.addClass("come-in"); 
-    } 
+      el.addClass("come-in");
+    }
   });
-  
+
 });
+
+
