83d4b6cae25cae5d18c32d72de70f1fb06ff8914
Roscrach <lilaprashadsharma@gmail.com>
2019-04-20 17:05:04 +0530

    Modification on colors and shadow

--- public/js/main.js
+++ public/js/main.js
@@ -199,12 +199,15 @@ $(document).ready(function () {
 
 });
 
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "100vw";
+  document.getElementById("sidenavs").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("sidenavs").style.display = "block";
 }
