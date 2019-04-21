c45fbb0c7e337ca04ec5c81775b69932ba41bec6
Roscrach <lilaprashadsharma@gmail.com>
2019-04-21 23:00:09 +0530

    Add move to top button

--- public/js/main.js
+++ public/js/main.js
@@ -231,3 +231,19 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("sidenavs").style.display = "inline-block";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
