bf3c03317ec59301bf92a2fe89425582945d1fd8
Roscrach <lilaprashadsharma@gmail.com>
2019-04-24 17:34:06 +0530

    Add individual item page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get("/blogs", (req, res) => {
     res.send("Blogs");
 });
 
+router.get("/item", (req, res) => {
+    res.render("item");
+});
+
 router.get("/products", (req, res) => {
     res.render("products");
 });
