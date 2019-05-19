8ea7b2a4d47ecb0543d9dc14711f6c22ac4d31aa
Roscrach <lilaprashadsharma@gmail.com>
2019-05-19 12:51:13 +0530

    Added development environment for ssl

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const server   = require("./imports/express"),
-        PORT   = 443,
-    mongoose   = require("mongoose");
+const server = require("./imports/express"),
+    PORT = 443,
+    mongoose = require("mongoose");
 var https = require('https');
 var fs = require('fs');
 const express = require("express");
-if(typeof process.env.NODE_ENV === "undefined"){
+if (typeof process.env.NODE_ENV === "undefined") {
     mongoose.connect("mongodb://admin:311210187@dev-shard-00-00-cbuvl.mongodb.net:27017,dev-shard-00-01-cbuvl.mongodb.net:27017,dev-shard-00-02-cbuvl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true", {
         useNewUrlParser: true
     }, (err) => {
@@ -15,7 +15,7 @@ if(typeof process.env.NODE_ENV === "undefined"){
             console.log("Database connected to Production environment!");
         }
     });
-}else{
+} else {
     mongoose.connect("mongodb+srv://admin:311210187@dev-cbuvl.mongodb.net/beta?retryWrites=true", {
         useNewUrlParser: true
     }, (err) => {
@@ -42,14 +42,22 @@ server.use(require("./routes/download"));
 
 //Wildcard route//
 //#server.get("*", (req, res) => {
-	//#    res.render("error", {message: "URL Not found!", code: 404});
-	//#});
+//#    res.render("error", {message: "URL Not found!", code: 404});
+//#});
 
+if (typeof process.env.NODE_ENV === "undefined") {
+    var options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/cert.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/chain.pem')
+    };
 
-var options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/chain.pem')
-};
+    https.createServer(options, server).listen(443, () => {
+        console.log("Server is on production mode and listening on 443");
+    });
+}else{
+    server.listen(3000, () => {
+        console.log("Server listening on PORT 3000");
+    })
+}
 
-https.createServer(options, server).listen(443);
