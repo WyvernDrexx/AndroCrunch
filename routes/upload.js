f1eb627e1fca7b45db1467473bce69e9766c8e27
Roscrach <lilaprashadsharma@gmail.com>
2019-05-29 19:37:30 +0530

    testing

--- routes/upload.js
+++ routes/upload.js
@@ -309,7 +309,7 @@ router.post("/files/:mimetype/thumbnail/:id", isLoggedIn, (req, res) => {
 
     thumbnail(req, res, (err) => {
 
-        if(req.file.mimetype.split("/")[1] === "jpeg" || req.file.mimetype.split("/")[0] !== "image"){
+        if(req.file.mimetype.split("/")[0] !== "image"){
             req.flash("error", "Image format unsupported: " + req.file.mimetype);
             res.redirect("back");
             deleteFromSystem("thumbnail", req.file.filename);
