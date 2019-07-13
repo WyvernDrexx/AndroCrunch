79ebca998d90c9c5f67f447a744649eca8c79e75
Roscrach <lilaprashadsharma@gmail.com>
2019-07-13 08:42:58 -0400

    node env check

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const server = require("./imports/express"),
+    PORT = 443,
     mongoose = require("mongoose");
+var https = require('https');
 var fs = require('fs');
-if (typeof process.env.NODE_ENV === "undefined") {
+const express = require("express");
+if (typeof process.env.NODE_ENV === "production") {
     mongoose.connect("mongodb://admin:311210187@dev-shard-00-00-cbuvl.mongodb.net:27017,dev-shard-00-01-cbuvl.mongodb.net:27017,dev-shard-00-02-cbuvl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true", {
         useNewUrlParser: true
     }, (err) => {
         if (err) {
-            console.log("Couldn't connect to database from Development environment!");
+            console.log("Couldn't connect to database from Production environment!");
             console.log(err);
         } else {
             console.log("Database connected to Production environment!");
@@ -20,7 +23,7 @@ if (typeof process.env.NODE_ENV === "undefined") {
             console.log("Couldn't connect to database from Development environment!");
             console.log(err);
         } else {
-            console.log("Database connected to Production environment!");
+            console.log("Database connected to Development environment!");
         }
     });
 }
@@ -47,10 +50,12 @@ if (typeof process.env.NODE_ENV === "undefined") {
         cert: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/cert.pem'),
         ca: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/chain.pem')
     };
-    console.log("[+] Production mode");
+    https.createServer(options, server).listen(443, () => {
+        console.log("Server is on production mode and listening on 443");
+    });
+} else {
+    server.listen(3000, () => {
+        console.log("Server listening on PORT 3000");
+    });
 }
-
-server.listen(3000, () => {
-    console.log("Server listening on PORT 3000");
-});
 
