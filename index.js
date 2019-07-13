cf9ba15fe48d44fad2d32faba58734cf8ff1a95c
Roscrach <lilaprashadsharma@gmail.com>
2019-07-13 08:32:21 -0400

    Listen on port 3000 even on production mode

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const server = require("./imports/express"),
-    PORT = 443,
     mongoose = require("mongoose");
-var https = require('https');
 var fs = require('fs');
-const express = require("express");
 if (typeof process.env.NODE_ENV === "undefined") {
     mongoose.connect("mongodb://admin:311210187@dev-shard-00-00-cbuvl.mongodb.net:27017,dev-shard-00-01-cbuvl.mongodb.net:27017,dev-shard-00-02-cbuvl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true", {
         useNewUrlParser: true
@@ -50,12 +47,10 @@ if (typeof process.env.NODE_ENV === "undefined") {
         cert: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/cert.pem'),
         ca: fs.readFileSync('/etc/letsencrypt/live/androcrunch.in/chain.pem')
     };
-    https.createServer(options, server).listen(443, () => {
-        console.log("Server is on production mode and listening on 443");
-    });
-} else {
-    server.listen(3000, () => {
-        console.log("Server listening on PORT 3000");
-    });
+    console.log("[+] Production mode");
 }
+
+server.listen(3000, () => {
+    console.log("Server listening on PORT 3000");
+});
 
