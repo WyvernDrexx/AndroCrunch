66ccdc57c7f1100ca09e26fbe03c4e38abc6792d
Roscrach <lilaprashadsharma@gmail.com>
2019-07-14 02:25:27 -0400

    small seo

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get("/", (req, res) => {
 
         res.render("index", {
           title:
-            "AndroCrunch | Beyond The Infinity |  Official Website of AndroCrunch Youtube",
+            "AndroCrunch Andro Crunch | Beyond The Infinity |  Official Website of AndroCrunch Youtube",
           trending: data[0].list.trending,
           latest,
           moment,
