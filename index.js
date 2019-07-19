cb827fae3fc591ba09e87d56f5d6a68a5b2daabf
Roscrach <lilaprashadsharma@gmail.com>
2019-07-19 10:19:55 +0530

    small change

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const server = require("./imports/express"),
-  mongoose = require("mongoose");
+    mongoose = require("mongoose");
 var https = require("https");
 var fs = require("fs");
 var PORT = 3000;
 
-if (typeof process.env.NODE_ENV == "undefined" || process.env.NODE_ENV === "production" ) {
-  mongoose.connect(
-    "mongodb://admin:311210187@dev-shard-00-00-cbuvl.mongodb.net:27017,dev-shard-00-01-cbuvl.mongodb.net:27017,dev-shard-00-02-cbuvl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true",
-    {
-      useNewUrlParser: true
-    },
-    err => {
-      if (err) {
-        console.log(
-          "Couldn't connect to database from Production environment!"
-        );
-        console.log(err);
-      } else {
-        console.log("Database connected to Production environment!");
-      }
-    }
-  );
+if (typeof process.env.NODE_ENV == "undefined" || process.env.NODE_ENV === "production") {
+    mongoose.connect(
+        "mongodb://admin:311210187@dev-shard-00-00-cbuvl.mongodb.net:27017,dev-shard-00-01-cbuvl.mongodb.net:27017,dev-shard-00-02-cbuvl.mongodb.net:27017/test?ssl=true&replicaSet=dev-shard-0&authSource=admin&retryWrites=true",
+        {
+            useNewUrlParser: true
+        },
+        err => {
+            if (err) {
+                console.log(
+                    "Couldn't connect to database from Production environment!"
+                );
+                console.log(err);
+            } else {
+                console.log("Database connected to Production environment!");
+            }
+        }
+    );
 } else {
-  mongoose.connect(
-    "mongodb+srv://admin:311210187@dev-cbuvl.mongodb.net/beta?retryWrites=true",
-    {
-      useNewUrlParser: true
-    },
-    err => {
-      if (err) {
-        console.log(
-          "Couldn't connect to database from Development environment!"
-        );
-        console.log(err);
-      } else {
-        console.log("Database connected to Development environment!");
-      }
-    }
-  );
+    mongoose.connect(
+        "mongodb+srv://admin:311210187@dev-cbuvl.mongodb.net/beta?retryWrites=true",
+        {
+            useNewUrlParser: true
+        },
+        err => {
+            if (err) {
+                console.log(
+                    "Couldn't connect to database from Development environment!"
+                );
+                console.log(err);
+            } else {
+                console.log("Database connected to Development environment!");
+            }
+        }
+    );
 }
 
 const serverFavicon = require("serve-favicon");
@@ -52,32 +52,32 @@ server.use(require("./routes/category"));
 server.use(require("./routes/download"));
 
 server.get("*", (req, res) => {
-  res.render("error", { message: "URL Not found!", code: 404 });
+    res.render("error", { message: "URL Not found!", code: 404 });
 });
 
 if (typeof process.env.NODE_ENV === "undefined") {
-  var options = {
-    key: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/cert.pem"),
-    ca: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/chain.pem")
-  };
-  https.createServer(options, server).listen((PORT = 443), () => {
-    console.log(`Server is on ${process.env.NODE_ENV} mode and listening on ${PORT}`);
-  });
-  
-  var http = require("http");
-  http
-    .createServer(function(req, res) {
-      res.writeHead(301, {
-        Location: "https://www" + req.headers["host"] + req.url
-      });
-      res.end();
-    })
-    .listen(80, () => {
-      console.log("[+] Another server listening on port 80");
+    var options = {
+        key: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/privkey.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/cert.pem"),
+        ca: fs.readFileSync("/etc/letsencrypt/live/androcrunch.in/chain.pem")
+    };
+    https.createServer(options, server).listen((PORT = 443), () => {
+        console.log(`Server is on ${process.env.NODE_ENV} mode and listening on ${PORT}`);
     });
+
+    var http = require("http");
+    http
+        .createServer(function (req, res) {
+            res.writeHead(301, {
+                Location: "https://www." + req.headers["host"] + req.url
+            });
+            res.end();
+        })
+        .listen(80, () => {
+            console.log("[+] Another server listening on port 80");
+        });
 } else {
-  server.listen(PORT, () => {
-    console.log(`Server listening on ${process.env.NODE_ENV} and PORT ${PORT}`);
-  });
+    server.listen(PORT, () => {
+        console.log(`Server listening on ${process.env.NODE_ENV} and PORT ${PORT}`);
+    });
 }
